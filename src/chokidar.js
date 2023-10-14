65655565afd73e7f9c6e4389a3c2aff24aedcffb
Anass <a.moussadi03@gmail.com>
2023-10-15 00:36:21 +0200

    compile error fix

--- src/chokidar.js
+++ src/chokidar.js
@@ -1,34 +1,57 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const chokidar_1 = __importDefault(require("chokidar"));
 const path_1 = __importDefault(require("path"));
 const child_process_1 = require("child_process");
+const fs_1 = __importDefault(require("fs"));
 const demoDirectory = "PLACEHOLDER";
 const parserScript = path_1.default.join(__dirname, "parser.ts");
 const watcher = chokidar_1.default.watch(demoDirectory, {
-    ignored: /(^|[/\\])\../,
-    persistent: true,
+  ignored: /(^|[/\\])\../,
+  persistent: true,
 });
+let isParsing = false;
 watcher
-    .on("add", (filePath) => {
+  .on("add", (filePath) => {
     if (filePath.endsWith(".dem")) {
-        console.log(`New demo file detected: ${filePath}`);
-        // Trigger parsing process
-        const parserProcess = (0, child_process_1.spawn)("node", [parserScript, filePath]);
-        parserProcess.on("exit", (code, signal) => {
-            if (code === 0) {
+      console.log(`New demo file detected: ${filePath}`);
+      if (!isParsing) {
+        isParsing = true;
+        console.log(`Waiting for demo to be ready...`);
+        let previousSize = 0;
+        let checkInterval = setInterval(() => {
+          const currentSize = fs_1.default.statSync(filePath).size;
+          if (previousSize === currentSize) {
+            clearInterval(checkInterval);
+            console.log("Starting parsing process");
+            // Trigger parsing process
+            const parserProcess = (0, child_process_1.spawn)("node", [
+              parserScript,
+              filePath,
+            ]);
+            parserProcess.on("exit", (code, signal) => {
+              if (code === 0) {
                 console.log("Parsing process completed successfully.");
-            }
-            else {
-                console.error(`Parsing process exited with code ${code} and signal ${signal}`);
-            }
-        });
+              } else {
+                console.error(
+                  `Parsing process exited with code ${code} and signal ${signal}`
+                );
+              }
+              isParsing = false;
+            });
+          } else {
+            previousSize = currentSize;
+          }
+        }, 4000);
+      }
     }
-})
-    .on("error", (error) => {
+  })
+  .on("error", (error) => {
     console.error(`Watcher error: ${error}`);
-});
+  });
 console.log(`Watching directory: ${demoDirectory}`);
--- src/parser.js
+++ src/parser.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
 const demofile_1 = __importDefault(require("demofile"));
@@ -9,24 +11,30 @@ const path_1 = __importDefault(require("path"));
 const demoFilePath = process.argv[2];
 const demoFile = new demofile_1.default.DemoFile();
 demoFile.on("start", () => {
-    console.log("Parsing demo file...");
+  console.log("Parsing demo file...");
 });
 demoFile.on("end", () => {
-    console.log("Demo file parsing complete.");
-    const players = demoFile.entities.players;
-    const extractedData = players.map((player) => ({
-        name: player.name,
-        steamId: player.steamId,
-    }));
-    const jsonFileName = path_1.default.basename(demoFilePath, ".dem") + ".json";
-    const jsonFilePath = path_1.default.join(path_1.default.dirname(demoFilePath), jsonFileName);
-    fs_1.default.writeFileSync(jsonFilePath, JSON.stringify(extractedData, null, 2));
-    console.log(`Data saved to ${jsonFilePath}`);
+  console.log("Demo file parsing complete.");
+  const players = demoFile.entities.players;
+  const extractedData = players.map((player) => ({
+    name: player.name,
+    steamId: player.steamId,
+  }));
+  const jsonFileName = path_1.default.basename(demoFilePath, ".dem") + ".json";
+  const jsonFilePath = path_1.default.join(
+    path_1.default.dirname(demoFilePath),
+    jsonFileName
+  );
+  fs_1.default.writeFileSync(
+    jsonFilePath,
+    JSON.stringify(extractedData, null, 2)
+  );
+  console.log(`Data saved to ${jsonFilePath}`);
 });
 fs_1.default.readFile(demoFilePath, (err, data) => {
-    if (err) {
-        console.log(`Error reading demo file: ${err}`);
-        return;
-    }
-    demoFile.parse(data);
+  if (err) {
+    console.log(`Error reading demo file: ${err}`);
+    return;
+  }
+  demoFile.parse(data);
 });
