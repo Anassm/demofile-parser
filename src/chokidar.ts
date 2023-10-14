117f90d324f4eb8ae8b85dc0879ecc06c583d314
Anass <a.moussadi03@gmail.com>
2023-10-15 00:16:38 +0200

    Added check for demo/match to be completed

--- src/chokidar.ts
+++ src/chokidar.ts
@@ -1,6 +1,7 @@
 import chokidar from "chokidar";
 import path from "path";
 import { spawn } from "child_process";
+import fs from "fs";
 
 const demoDirectory = "PLACEHOLDER";
 const parserScript = path.join(__dirname, "parser.ts");
@@ -10,23 +11,45 @@ const watcher = chokidar.watch(demoDirectory, {
   persistent: true,
 });
 
+let isParsing = false;
+
 watcher
   .on("add", (filePath) => {
     if (filePath.endsWith(".dem")) {
       console.log(`New demo file detected: ${filePath}`);
 
-      // Trigger parsing process
-      const parserProcess = spawn("node", [parserScript, filePath]);
+      if (!isParsing) {
+        isParsing = true;
 
-      parserProcess.on("exit", (code, signal) => {
-        if (code === 0) {
-          console.log("Parsing process completed successfully.");
-        } else {
-          console.error(
-            `Parsing process exited with code ${code} and signal ${signal}`
-          );
-        }
-      });
+        console.log(`Waiting for demo to be ready...`);
+
+        let previousSize = 0;
+        let checkInterval = setInterval(() => {
+          const currentSize = fs.statSync(filePath).size;
+
+          if (previousSize === currentSize) {
+            clearInterval(checkInterval);
+
+            console.log("Starting parsing process");
+
+            // Trigger parsing process
+            const parserProcess = spawn("node", [parserScript, filePath]);
+
+            parserProcess.on("exit", (code, signal) => {
+              if (code === 0) {
+                console.log("Parsing process completed successfully.");
+              } else {
+                console.error(
+                  `Parsing process exited with code ${code} and signal ${signal}`
+                );
+              }
+              isParsing = false;
+            });
+          } else {
+            previousSize = currentSize;
+          }
+        }, 4000);
+      }
     }
   })
   .on("error", (error) => {
